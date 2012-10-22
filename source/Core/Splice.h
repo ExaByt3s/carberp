ff819ee2c563d31e7254ccbd121913ddfc17d7db
Igor@RIVSOFT <rivsoft1975@mail.ru>
2012-10-22 21:35:26 +0300

    -

--- source/Core/Splice.h
+++ source/Core/Splice.h
@@ -3,3 +3,6 @@
 PVOID HookApi( DWORD DllNum, DWORD FuncHash, LPVOID ReplacementFunc );
 PVOID HookApi( DWORD DllNum, DWORD FuncHash, LPVOID ReplacementFunc, PVOID FuncReal );
 PVOID HookApi2( DWORD Dll, DWORD FuncVA, DWORD ReplacementFunc );
+//установка хука в нужной DllName модуля Module, т. е. модуль подгрузил DllName и прямо в таблице импорта
+//делается подмена
+bool PathIAT(PVOID Module,PCHAR DllName,PCHAR FuncName,PVOID NewHandler,PVOID *OldHandler);
